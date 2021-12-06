65bacc3e4c5f543806d6a62f1a92a4df60f7076e
CreepyEllie <creepyellie98@gmail.com>
2021-12-06 20:27:08 +0000

    initial upload

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias uu='sudo apt update && sudo apt upgrade'
+alias rm='rm -i'
+alias mv='mv -i'
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+starship init fish | source
+
+neofetch
